aede9861706ae6152fe1d09e5096c4dc676e983b
Chimev <chimevictor.CV@gmail.com>
2025-02-10 09:50:40 +0100

    removed obiex

--- src/container/Work/work.js
+++ src/container/Work/work.js
@@ -4,7 +4,7 @@ export const project = [
     {id: 1,  title: 'campusEase', description: 'My Personal Project ', imgUrl: images.campuseEase, url: 'https://campusease-v2.vercel.app', tag: 'Website App' },
     {id: 2,  title: 'Nexpixel', description: 'Tech company that provides a one-stop solution for clients seeking to establish a strong online presence and enhance their digital capabilities.', imgUrl: images.nexpixel, url: 'https://nexpixel-frontend.vercel.app/', tag: 'Website'},
     {id: 3, title: 'PAATEE', description: 'A Web Application that helps in organizing a party faster by connecting vendors to party planners or customers', imgUrl: images.paatee, url: 'https://paatee-web.vercel.app/', tag: 'Website App'},
-    {id: 4, title: 'Obiex', description: 'Obiex crypto app website', imgUrl: images.Obiex, url: 'https://www.obiex.finance/', tag: 'Website'},
+    // {id: 4, title: 'Obiex', description: 'Obiex crypto app website', imgUrl: images.Obiex, url: 'https://www.obiex.finance/', tag: 'Website'},
     {id: 5, title: 'CDI', description: 'CDI nation Website', imgUrl: images.CDI, url: 'https://cdi-nation.vercel.app/', tag: 'Website'},
     {id: 6, title: 'Chronical Strategy Partners', description: 'Help small business scale', imgUrl: images.Chronicle, url: 'https://chroniclestrategy.com', tag: 'Website'},
   ]
